f25d04b5d204b39075a8000b179caf7c9ba4edd5
keeganridgley107 <keeganwridgley@gmail.com>
2019-10-30 08:23:33 -0600

    remove nltk download script, ran in terminal

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
+
 import numpy
 import tflearn
-import tensorflow 
+import tensorflow
 import random
 import json
 
@@ -11,3 +12,19 @@ with open("intents.json") as file:
     data = json.load(file)
 
 print(data)
+
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        token_words = nltk.word_tokenize(pattern)
+        words.extend(token_words)
+        docs.append(pattern)
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+print(labels)
+
